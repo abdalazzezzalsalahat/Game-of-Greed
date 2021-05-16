8abba70794c8f0075ec16c44eff6c6a852ed8dc3
abdalazez <abdalazzezzalsalahat@icloud.com>
2021-05-16 12:51:39 +0200

    startup code

--- tests/test_banker.py
+++ tests/test_banker.py
@@ -3,20 +3,20 @@ from game_of_greed.banker import Banker
 
 pytestmark = [pytest.mark.version_1, pytest.mark.version_2]
 
-
+@pytest.mark.skip
 def test_new_banker():
     banker = Banker()
     assert banker.balance == 0
     assert banker.shelved == 0
 
-
+@pytest.mark.skip
 def test_shelf():
     banker = Banker()
     banker.shelf(100)
     assert banker.shelved == 100
     assert banker.balance == 0
 
-
+@pytest.mark.skip
 def test_deposit():
     banker = Banker()
     banker.shelf(100)
