e9588fb477eca678c57201a7f2579da3a966567d
abdalazez <abdalazzezzalsalahat@icloud.com>
2021-05-20 20:30:04 +0200

    solved all problems

--- game_of_greed/game.py
+++ game_of_greed/game.py
@@ -2,12 +2,11 @@
 from collections import Counter
 # import sys
 
+# from game_logic import GameLogic
+# from banker import Banker
 
-from game_logic import GameLogic
-from banker import Banker
-
-# from game_of_greed.game_logic import GameLogic
-# from game_of_greed.banker import Banker
+from game_of_greed.game_logic import GameLogic
+from game_of_greed.banker import Banker
 
 
 
